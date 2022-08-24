8532f7e964992bf0fc792be13957ce1444761987
ZohaibFattani <102203555+ZohaibFattani@users.noreply.github.com>
2022-08-23 21:11:23 -0700

    Add files via upload

---
+++ game.js
@@ -0,0 +1,14 @@
+// Create the playable character(GOKU)
+
+// Create a non-playable character(His Freinds/Family)
+
+//add new images
+
+//add new NPC
+
+// each npc start at different locations and take different routes
+
+//code to move PC with keyboard
+
+//code to stop game if NPC is hit
+
