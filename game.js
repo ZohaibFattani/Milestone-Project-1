ecffd5b5d9aced729f955baa792ae668975de520
ZohaibFattani <102203555+ZohaibFattani@users.noreply.github.com>
2022-08-24 01:08:04 -0700

    Add files via upload

--- game.js
+++ game.js
@@ -1,5 +1,32 @@
 // Create the playable character(GOKU)
+function newPlayableCharacter(x, y) {
+    const element = newImage('assets/green-character/static.gif')
+    element.style.zIndex = 1;
 
+    function handleDirectionChange(direction) {
+        if (direction === null) {
+            element.src = `assets/GokuStop.webp`
+        }
+        if (direction === 'west') {
+            element.src = `assets/GokuEast.gif`
+        }
+        if (direction === 'north') {
+            element.src = `assets/GokuEast.gif`
+        }
+        if (direction === 'east') {
+            element.src = `assets/GokuEast.gif`
+        }
+        if (direction === 'south') {
+            element.src = `assets/GokuEast.gif`
+        }
+    }
+
+    move(element).withArrowKeys(x, y, handleDirectionChange)
+
+    return {
+        element: element
+    }
+}
 // Create a non-playable character(His Freinds/Family)
 
 //add new images
